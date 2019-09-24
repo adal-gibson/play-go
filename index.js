d27ba339e92b11678fa8183c4eeb6c6a3ab6e318
Madeleine Young <madeleine.rgr.young@gmail.com>
2019-09-24 13:04:12 -0400

    have line labels

--- index.js
+++ index.js
@@ -3,54 +3,54 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.use(express.static('jquery'));
-app.use(express.static('styles'));
+app.use(express.static('jquery')); // sets path for jquery folder
+app.use(express.static('styles')); // sets path for styles folder
 
-
+// loads index.html
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-
+// controls what happens when a user connects
 io.on('connection', function(socket){
     console.log('a user connected');
+
+    // https://ayushgp.github.io/Tic-Tac-Toe-Socket-IO/
+    socket.on('createGame', function(data){
+        socket.join('room-' + ++rooms);
+        socket.emit('newGame', {name: data.name, room: 'room-'+rooms});
+    });
+
+    socket.on('joinGame', function(data){
+        var room = io.nsps['/'].adapter.rooms[data.room];
+        if( room && room.length == 1){
+            socket.join(data.room);
+            socket.broadcast.to(data.room).emit('player1', {});
+            socket.emit('player2', {name: data.name, room: data.room });
+        }
+        else {
+            socket.emit('err', {message: 'Sorry, The room is full!'});
+        }
+    });
+
+    socket.on('playTurn', function(data){
+        socket.broadcast.to(data.room).emit('turnPlayed', {
+            node: data.node,
+            room: data.room
+        });
+    });
+
+    socket.on('gameEnded', function(data){
+        socket.broadcast.to(data.room).emit('gameEnd', data);
+    });
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
 });
 
 
-// https://ayushgp.github.io/Tic-Tac-Toe-Socket-IO/
-socket.on('createGame', function(data){
-    socket.join('room-' + ++rooms);
-    socket.emit('newGame', {name: data.name, room: 'room-'+rooms});
-});
 
-
-socket.on('joinGame', function(data){
-    var room = io.nsps['/'].adapter.rooms[data.room];
-    if( room && room.length == 1){
-        socket.join(data.room);
-        socket.broadcast.to(data.room).emit('player1', {});
-        socket.emit('player2', {name: data.name, room: data.room });
-    }
-    else {
-        socket.emit('err', {message: 'Sorry, The room is full!'});
-    }
-});
-
-
-socket.on('playTurn', function(data){
-    socket.broadcast.to(data.room).emit('turnPlayed', {
-        node: data.node,
-        room: data.room
-    });
-});
-
-
-socket.on('gameEnded', function(data){
-    socket.broadcast.to(data.room).emit('gameEnd', data);
-});
 
 
 // from socket.io documentation
--- jquery/board.js
+++ jquery/board.js
@@ -2,17 +2,22 @@ $(document).ready(function(){
 
     // generates the board
     $('#board').ready(function() {
-        var dim = 9; // the dimension of the board
+        var dim = 9; // the dimensions of the board
         var board = $('<svg class="board" width="100%" height="100%" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg"/>'); // means html will be appended in a div
-        var start = 6; // the percentage padding that svg needs to not cut off nodes
-        var end = 94; // same percentage that needs to be added
+        var start = 8; // the percentage padding that svg needs to not cut off nodes
+        var end = 92; // same percentage that needs to be added
         var gap = (end-start)/(dim-1); // establishes the space between the nodes
         var radius = 4.5; // the radius of the circles
-        var id = 0;
-        var clickCount = 0;
+        var id = 0; // id of the first node
+        var clickCount = 0; // SUPER HACKY - GOTTA GET RID OF THIS
+        var asciiA = 65;
 
         // adds lines first so that they are always behind the nodes
         for(var i = 0; i < dim; i++) {
+            board.append('<text x="'+ 2 +'" y="'+ (start+(gap*(i))+(gap/11)) +'" font-size="'+ gap/3 +'">'+ (dim-i) +' </text>') // numbers - on left
+            board.append('<text x="'+ 95 +'" y="'+ (start+(gap*(i))+(gap/11)) +'" font-size="'+ gap/3 +'">'+ (dim-i) +' </text>') // numbers - on right
+            board.append('<text x="'+ (start+(gap*(i))-(gap/11)) +'" y="'+ 5 +'" font-size="'+ gap/3 +'">'+ String.fromCharCode(asciiA + i) +' </text>') // letters - top
+            board.append('<text x="'+ (start+(gap*(i))-(gap/11)) +'" y="'+ 98 +'" font-size="'+ gap/3 +'">'+ String.fromCharCode(asciiA + i) +' </text>') // letters - bottom
             board.append('<line x1="'+ (start+gap*i) +'" y1="'+ (start) +'" x2="'+ (start+gap*i) +'" y2="'+ (end) +'" />'); // vertical lines
             board.append('<line x1="'+ (start) +'" y1="'+ (start+gap*i) +'" x2="'+ (end) +'" y2="'+ (start+gap*i) +'" />'); // horizontal lines
         }
